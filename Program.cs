524b37d98e2e98a8b728cb770e2be841b2b9ef92
Jefferson Nascimento <jf.nts10@gmail.com>
2024-11-16 16:09:03 -0300

    feat: implements the solution

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criando instância da classe Nokia
+            Nokia nokia = new Nokia("123456789", "ModeloNokia", "111111111111111", 64);
+            Console.WriteLine("Testando Nokia:");
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            // Criando instância da classe Iphone
+            Iphone iphone = new Iphone("987654321", "ModeloIphone", "222222222222222", 128);
+            Console.WriteLine("\nTestando iPhone:");
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
